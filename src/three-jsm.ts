2858313e7dda56f521dd62a0542501d749cf431e
Alejandro Alvarado <alvarado.a@smile80.it>
2019-12-10 11:42:32 +0100

    Added lines

--- src/three-jsm.ts
+++ src/three-jsm.ts
@@ -5,10 +5,33 @@
 const THREE = require('three')
 window.THREE = THREE || {}
 
+// Controls
 const controls = require('three/examples/jsm/controls/OrbitControls')
 
 const OrbitControls = controls.OrbitControls
 const MapControls = controls.MapControls
+
+// Loaders
 const GLTFLoader = require('three/examples/jsm/loaders/GLTFLoader')
 
-export { OrbitControls, MapControls, GLTFLoader }
+// Lines
+const Line2 = require('three/examples/jsm/lines/Line2').Line2
+const LineGeometry = require('three/examples/jsm/lines/LineGeometry').LineGeometry
+const LineMaterial = require('three/examples/jsm/lines/LineMaterial').LineMaterial
+const LineSegments2 = require('three/examples/jsm/lines/LineSegments2').LineSegments2
+const LineSegmentsGeometry = require('three/examples/jsm/lines/LineSegmentsGeometry').LineSegmentsGeometry
+const Wireframe = require('three/examples/jsm/lines/Wireframe').Wireframe
+const WireframeGeometry2 = require('three/examples/jsm/lines/WireframeGeometry2').WireframeGeometry2
+
+export {
+  OrbitControls,
+  MapControls,
+  GLTFLoader,
+  Line2,
+  LineGeometry,
+  LineMaterial,
+  LineSegments2,
+  LineSegmentsGeometry,
+  Wireframe,
+  WireframeGeometry2
+}
